3db8e1fba03f75c0d8fe7ad94c0c50bac61b0839
Stargazer <chris@antisphex.net>
2019-03-14 15:19:50 +0100

    work on jpa model

---
+++ src/main/java/de/smarthome/assistant/shoppinglist/model/ShoppingList.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019. Chris Wohlbrecht
+ *
+ * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.smarthome.assistant.shoppinglist.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shoppinglist")
+public class ShoppingList {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "shoppingList_Sequence")
+    @SequenceGenerator(name = "shoppingList_Sequence", sequenceName = "SHOPPINGLIST_SEQ")
+    private Long id;
+
+    @ManyToMany(mappedBy = "shoppingLists")
+    private List<Product> product;
+
+    @Column(name = "date")
+    private LocalDate localDate;
+
+}
