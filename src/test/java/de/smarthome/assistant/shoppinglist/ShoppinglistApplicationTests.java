d104c7e237119eda1911835e9eba76c8eeba21d0
Stargazer <chris@antisphex.net>
2019-04-07 21:47:15 +0200

    WIP

--- src/test/java/de/smarthome/assistant/shoppinglist/ShoppinglistApplicationTests.java
+++ src/test/java/de/smarthome/assistant/shoppinglist/ShoppinglistApplicationTests.java
@@ -2,11 +2,17 @@ package de.smarthome.assistant.shoppinglist;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@ActiveProfiles("test")
 public class ShoppinglistApplicationTests {
 
 	@Test
